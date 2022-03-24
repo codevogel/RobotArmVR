5adc0dadcd8dda5dc2e20c9d562ef11d76184791
Aslesh <cjullienmallet@gmail.com>
2022-03-24 15:49:22 +0100

    removed test code

--- Assets/Scripts/Holograms.cs
+++ Assets/Scripts/Holograms.cs
@@ -5,21 +5,9 @@ using UnityEngine;
 public class Holograms : MonoBehaviour
 {
     [SerializeField] private Material hologramMaterial;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Here");
         other.transform.GetComponent<MeshRenderer>().material =hologramMaterial;   
     }
 }
