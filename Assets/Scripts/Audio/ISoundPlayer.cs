f0e291003b56c85ca6699392d18c8c0575c01914
VRStudent <luca.guagliardo@live.nl>
2022-04-19 11:23:22 +0200

    added more types of sound players

---
+++ Assets/Scripts/Audio/ISoundPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+// Alias om verwarring tussen System.Random en UnityEngine te voorkomen
+public interface ISoundPlayer
+{
+    /// <summary>
+    /// Plays a clip on the audio source.
+    /// </summary>
+    void PlayClip();
+
+    /// <summary>
+    /// Plays a clip on interval on the audio source
+    /// </summary>
+    /// <remarks>
+    /// Use with coroutines.
+    /// </remarks>
+    /// <param name="minInterval"> The minimum time in seconds before the sound can be played again.</param>
+    /// <param name="maxInterval"> The maximum time in seconds before the sound can be played again.</param>
+    /// <param name="playImmediately"> Determines if a clip should be played before a start interval period.</param>
+    IEnumerator PlayClipOnInterval(float minInterval, float maxInterval, bool playImmediately = false);
+}
--- Assets/Scripts/Audio/SoundPlayer.cs
+++ Assets/Scripts/Audio/SoundPlayer.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -6,17 +6,13 @@ using UnityEngine.Assertions;
 using UnityRandom = UnityEngine.Random;
 
 [RequireComponent(typeof(AudioSource))]
-public class RandomSoundPlayer : MonoBehaviour, ISoundPlayer
+public class SoundPlayer : MonoBehaviour, ISoundPlayer
 {
-    [SerializeField] private AudioClip[] _clips;
-
     private AudioSource _audioSource;
 
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
-
-        Assert.IsNotNull(_audioSource);
     }
 
     /// <summary>
@@ -25,18 +21,13 @@ public class RandomSoundPlayer : MonoBehaviour, ISoundPlayer
     public void PlayClip()
     {
         Assert.IsNotNull(_audioSource.outputAudioMixerGroup, $"No mixer group in {this.gameObject.name}. Audio source should have a mixer group assigned in order to get the most control out of it.");
-        Assert.IsTrue(_clips.Length > 0, $"No audio clips in {this.name}. ");
+        Assert.IsNotNull(_audioSource.clip, $"No audio clip in {this.gameObject.name}.");
 
-#if UNITY_EDITOR
-        if (_audioSource.clip != null)
-            Debug.Log($"Audio clip set in the audio source of {this.gameObject.name} will be ignored and can be left empty.");
-#endif
-        
-        _audioSource.PlayOneShot(_clips[UnityRandom.Range(0, _clips.Length)]);
+        _audioSource.Play();
     }
 
     /// <summary>
-    /// Plays a random clip on interval on the audio source
+    /// Plays a clip on interval on the audio source
     /// </summary>
     /// <remarks>
     /// Use with coroutines.
@@ -47,23 +38,19 @@ public class RandomSoundPlayer : MonoBehaviour, ISoundPlayer
     public IEnumerator PlayClipOnInterval(float minInterval, float maxInterval, bool playImmediately = false)
     {
         Assert.IsNotNull(_audioSource.outputAudioMixerGroup, $"No mixer group in {this.gameObject.name} .Audio source should have a mixer group assigned in order to get the most control out of it.");
-        Assert.IsTrue(_clips.Length > 0, $"No audio clips in {this.name}.");
+        Assert.IsNotNull(_audioSource.clip, $"No audio clips in {this.gameObject.name}.");
         Assert.IsTrue(minInterval >= 0, "Minimum interval cannot be lower than 0 seconds.");
         Assert.IsTrue(minInterval <= maxInterval, "Minimum interval cannot be higher than the maximum interval.");
 
-#if UNITY_EDITOR
-        if (_audioSource.clip != null)
-            Debug.Log($"Audio clip set in the audio source of {this.name} will be ignored and can be left empty.");
-#endif
 
         if (playImmediately)
-            _audioSource.PlayOneShot(_clips[UnityRandom.Range(0, _clips.Length)]);
+            _audioSource.Play();
 
         while (true)
         {
-            yield return new WaitForSeconds(UnityRandom.Range(minInterval,maxInterval));
+            yield return new WaitForSeconds(UnityRandom.Range(minInterval, maxInterval));
 
-            _audioSource.PlayOneShot(_clips[UnityRandom.Range(0, _clips.Length)]);
+            _audioSource.Play();
         }
     }
 }
