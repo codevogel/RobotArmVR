df26f677f5fe4fe1b6733d0172ce9c9aaa062b9f
VRStudent <luca.guagliardo@live.nl>
2022-05-11 10:16:33 +0200

    extra documentatie

--- Assets/Scripts/Onboarding/TutorialGoalPositions.cs
+++ Assets/Scripts/Onboarding/TutorialGoalPositions.cs
@@ -13,24 +13,43 @@ public class TutorialGoalPositions : MonoBehaviour
     int _currentGoal;
     Coroutine _advanceCoroutine;
 
+    /// <summary>
+    /// Resets the active goal positions and starts with the first one.
+    /// </summary>
     public void Begin()
     {
         Assert.IsTrue(_colliders.Length > 0, $"({this.gameObject.name} {nameof(TutorialGoalPositions)}) Must have at least one collider goal.");
+
+#if UNITY_EDITOR
+        if(_currentGoal < _colliders.Length)
+#else
+        Assert.IsTrue(_currentGoal < _colliders.Length, $"{nameof(_currentGoal)} is in an illegal state");
+#endif
+            _colliders[_currentGoal].gameObject.SetActive(false);
 
         _currentGoal = 0;
         _colliders[0].gameObject.SetActive(true);
     }
 
+    /// <summary>
+    /// Starts a coroutine to advance to the next goal after a set period of time.
+    /// </summary>
     public void QueueAdvance() =>
         _advanceCoroutine = StartCoroutine(Advance());
 
+    /// <summary>
+    /// Cancels the coroutine to advance to the next goal after a set period of time.
+    /// </summary>
     public void CancelAdvance()
     {
         if (_advanceCoroutine != null)
             StopCoroutine(_advanceCoroutine);
     }
 
-    public IEnumerator Advance()
+    /// <summary>
+    /// Set the current goal to the next one in line after a set amount of time.
+    /// </summary>
+    private IEnumerator Advance()
     {
         if (timeRequiredInside > 0)
             yield return new WaitForSeconds(timeRequiredInside);
@@ -39,6 +58,7 @@ public class TutorialGoalPositions : MonoBehaviour
 
         _currentGoal++;
 
+        // check if this was the last goal
         if (_currentGoal == _colliders.Length)
         {
             _onCompletion.Invoke();
