7d073e98cab010da201f2112398b40e887c3ee19
Aslesh <cjullienmallet@gmail.com>
2022-05-23 11:05:48 +0200

    added idle state to robotassistant animator

--- Assets/Scripts/Managers/TrainingScriptManager.cs
+++ Assets/Scripts/Managers/TrainingScriptManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Playables;
-using static UIHoverButton;
 
 public class TrainingScriptManager : MonoBehaviour
 {
