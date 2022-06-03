6a1bb50adb08e4ccd82af4c15f89f8f88775fca5
Aslesh <cjullienmallet@gmail.com>
2022-06-03 14:00:57 +0200

    removed blue ray when pointing

--- Assets/Scripts/Controls/PointAction.cs
+++ Assets/Scripts/Controls/PointAction.cs
@@ -10,68 +10,10 @@ public class PointAction : MonoBehaviour
     [SerializeField] private XRRayInteractor rayInteractor;
     [SerializeField] private GameObject confirmationCanvas;
 
-    private bool isPointing;
     private Transform lastCollision;
-
-    private Gradient pointGradient;
-    private GradientColorKey[] pointColorKey;
-    private GradientAlphaKey[] alphaColorKey;
-
-    private Gradient validGradient;
-    private Gradient invalidGradient;
 
     [SerializeField]
     private bool rayInteractorEnabled;
-
-    private void Start()
-    {
-        if (rayInteractorEnabled)
-        {
-            rayInteractor.transform.gameObject.SetActive(false);
-        }
-        pointGradient = new Gradient();
-
-        pointColorKey = new GradientColorKey[2];
-        pointColorKey[0].color = Color.blue;
-        pointColorKey[0].time = 0;
-        pointColorKey[1].color = Color.blue;
-        pointColorKey[1].time = 1;
-
-        alphaColorKey = new GradientAlphaKey[2];
-        alphaColorKey[0].alpha = 1;
-        alphaColorKey[0].time = 0;
-        alphaColorKey[1].alpha = 1;
-        alphaColorKey[1].time = 0;
-
-        pointGradient.SetKeys(pointColorKey, alphaColorKey);
-
-        if (rayInteractorEnabled)
-        {
-            validGradient = rayInteractor.transform.GetComponent<XRInteractorLineVisual>().validColorGradient;
-            invalidGradient = rayInteractor.transform.GetComponent<XRInteractorLineVisual>().invalidColorGradient;
-        }
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (isPointing&& rayInteractorEnabled)
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(rayInteractor.transform.position, rayInteractor.transform.forward.normalized, out hit, rayInteractor.GetComponent<XRRayInteractor>().maxRaycastDistance)
-                && hit.transform.GetComponent<UIHoverButton>()!=null)
-            {
-                hit.transform.GetComponent<UIHoverButton>().TimerActive(true);
-                lastCollision = hit.transform;
-            }
-            else if (lastCollision!=null)
-            {
-                lastCollision.transform.GetComponent<UIHoverButton>().TimerActive(false);
-                lastCollision = null;
-            }
-        }
-    }
 
     public void ResetHoverCollision()
     {
@@ -93,27 +35,12 @@ public class PointAction : MonoBehaviour
             return;
         }
 
-        isPointing = point;
         if (point)
         {
-            if (rayInteractorEnabled)
-            {
-                rayInteractor.transform.GetComponent<XRInteractorLineVisual>().validColorGradient = pointGradient;
-                rayInteractor.transform.GetComponent<XRInteractorLineVisual>().invalidColorGradient = pointGradient;
-                rayInteractor.gameObject.SetActive(true);
-            }
-
-
             HandManager.Instance.ChangePose(HandPose.IDLE, HandPose.POINT, leftOrRight);
         }
         else
         {
-            if (rayInteractorEnabled)
-            {
-                rayInteractor.transform.GetComponent<XRInteractorLineVisual>().validColorGradient = validGradient;
-                rayInteractor.transform.GetComponent<XRInteractorLineVisual>().invalidColorGradient = invalidGradient;
-                rayInteractor.gameObject.SetActive(false);
-            }
             HandManager.Instance.ChangePose(HandPose.POINT, HandPose.IDLE, leftOrRight);
         }
     }
