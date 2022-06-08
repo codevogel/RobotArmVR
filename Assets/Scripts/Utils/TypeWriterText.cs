14d809f0829fb8ae39b47f763b079a9f279851b2
VRStudent <luca.guagliardo@live.nl>
2022-06-08 15:33:08 +0200

    Prevent spilling of text when the area gets cleared

--- Assets/Scripts/Utils/TypeWriterText.cs
+++ Assets/Scripts/Utils/TypeWriterText.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
+/// <summary>
+/// A mechanism to write text over a period of time in a TMP text field.
+/// </summary>
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class TypeWriterText : MonoBehaviour
 {
-    [SerializeField, Range(0.1f, 0.01f)]private float charWaitTime;
-    //[SerializeField]private string displayText;
+    /// <summary>
+    /// The time in seconds between each character appearing on screen.
+    /// </summary>
+    [SerializeField, Range(0.1f, 0.01f)] 
+    private float charWaitTime;
+
     private TextMeshProUGUI textHolder;
+    private Coroutine writeCoroutine;
 
     [ExecuteInEditMode]
     private void Awake()
@@ -16,27 +24,42 @@ public class TypeWriterText : MonoBehaviour
         textHolder.text = string.Empty;
     }
 
-    // Start is called before the first frame update
-    //void Start()
-    //{
-    //    textHolder = GetComponent<TextMeshProUGUI>();
-    //    textHolder.text = null;
-    //    StartCoroutine(TextChange());
-    //}
-
+    /// <summary>
+    /// Clears the text area and writes down the text over time.
+    /// </summary>
+    /// <param name="text"> The text to be written.</param>
     public void Write(string text)
     {
-        textHolder.text = string.Empty;
+        Clear();
 
         StartCoroutine(TextChange(text));
     }
 
+    /// <summary>
+    /// Appends the text to already displayed text in the area.
+    /// </summary>
+    /// <param name="text"> The text to be written.</param>
+    public void Append(string text)
+    {
+        StartCoroutine(TextChange(text));
+    }
+
+    /// <summary>
+    /// Stops the active writing coroutine and clears the text area of any text.
+    /// </summary>
     public void Clear()
     {
+        if (writeCoroutine != null)
+            StopCoroutine(writeCoroutine);
+
         textHolder.text = string.Empty;
     }
 
-    IEnumerator TextChange(string text)
+    /// <summary>
+    /// Appends characters over time to the text area on a set interval.
+    /// </summary>
+    /// <param name="text"> The text to be written.</param>
+    private IEnumerator TextChange(string text)
     {
         foreach (char c in text)
         {
