78d61162a6b733b28d55765f4a4e4c2eeed468a5
VRStudent <luca.guagliardo@live.nl>
2022-06-09 12:10:25 +0200

    billboarding on robot projection

---
+++ Assets/Scripts/Onboarding/LookAtPlayer.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+/// <summary>
+/// A behavior that forces the object to always look towards the player
+/// </summary>
+public class LookAtPlayer : MonoBehaviour
+{
+    /// <summary>
+    /// Determines if the rotation should be flipped.
+    /// </summary>
+    /// <remarks>
+    /// Used for things like UI Elements.
+    /// </remarks>
+    [field: SerializeField, Tooltip("Determines if the rotation should be flipped. Used for things like UI Elements.")]
+    public bool Flipped { get; set; }
+
+    private Transform _player;
+
+    /// <summary>
+    /// Registers the camera transform.
+    /// </summary>
+    private void Start()
+    {
+        _player = Camera.main.transform;
+    }
+
+    /// <summary>
+    /// Changes the object's transform to always look at the main camera.
+    /// </summary>
+    private void Update()
+    {
+        if (Flipped)
+        {
+            transform.rotation = Quaternion.LookRotation(transform.position - _player.position);
+        }
+        else
+        {
+            transform.LookAt(_player);
+        }
+    }
+}
