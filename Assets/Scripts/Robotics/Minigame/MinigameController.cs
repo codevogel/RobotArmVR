0d0a17807984a4cc3a99f80dd7b4997fc9d07b79
VRStudent <luca.guagliardo@live.nl>
2022-06-16 14:07:28 +0200

    documentatie toegevoegd

--- Assets/Scripts/Robotics/Minigame/MinigameController.cs
+++ Assets/Scripts/Robotics/Minigame/MinigameController.cs
@@ -1,58 +1,102 @@
+#if UNITY_ASSERTIONS
+using UnityEngine.Assertions;
+#endif
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
 using UnityEngine.Events;
 
+/// <summary>
+/// A mechanism to play a minigame where the player moves into positions while avoiding obstacles.
+/// </summary>
 public class MinigameController : MonoBehaviour
 {
+    /// <summary> 
+    /// The objects spawned by the minigame. 
+    /// </summary>
     readonly private List<OnTriggerColliderFilter> _authoredChildren = new List<OnTriggerColliderFilter>();
+
+    /// <summary> 
+    /// The coroutine that starts and ends the minigame. 
+    /// </summary>
     private Coroutine _minigameCoroutine;
+
+    /// <summary> 
+    /// The time at which the minigame has been started. 
+    /// </summary>
     private float _timeStarted;
 
+    /// <summary>
+    /// The current score of the minigame session.
+    /// </summary>
     public int Score { get; private set; }
+
+    /// <summary>
+    /// The time remaining until the minigame is over.
+    /// </summary>
     public float TimeRemaining => Mathf.Clamp(TimeLimit - (Time.time - _timeStarted), 0, float.MaxValue);
+
+    /// <summary>
+    /// Shows if the minigame is currently being played.
+    /// </summary>
     public bool IsBeingPlayed => _minigameCoroutine != null;
 
-    [field: SerializeField, Min(0)]
+    /// <summary>
+    /// The amount of time in seconds the player has to score points in a minigame session.
+    /// </summary>
+    [field: SerializeField, Min(0), Tooltip("The amount of time in seconds the player has to score points in a minigame session.")]
     public float TimeLimit { get; set; } = 120;
-    [SerializeField]
+
+    [SerializeField, Min(0), Tooltip("The amount of additional obstacles to try spawning in to increase difficulty.")]
+    private int _obstaclesPerRound;
+
+    [SerializeField, Tooltip("The pointer the player has to move into the goals to score points.")]
     private Collider _pointer;
 
     [Header("Bounds")]
-    [SerializeField, Min(float.Epsilon)]
+    [SerializeField, Min(float.Epsilon), Tooltip("The radius in which goals and obstacles will be spawned.")]
     private float _radius;
-    [SerializeField, Min(0)]
+
+    [SerializeField, Min(0), Tooltip("The radius from the center in which no objects can be spawned.")]
     private float _deadzone;
-    [SerializeField, Min(0)]
+
+    [SerializeField, Min(0), Tooltip("The vertical extend in which goals and obstacles can be spawned.")]
     private float _verticalRadius;
-    [SerializeField]
+
+    [SerializeField, Tooltip("The vertical offset to move the space objects can be spawned in up and down.")]
     private float _verticalOffset;
-    [SerializeField, Min(float.Epsilon)]
-    private float _pointerRadius;
 
-    [Header("Obstacles")]
-    [SerializeField, Min(0)]
-    private int _obstaclesPerRound;
-    //[SerializeField]
-    //private float _objectRadius;
-
-    [Header("Goal")]
-    [SerializeField]
+    [Header("Prefabs")]
+    [SerializeField, Tooltip("The prefab used as a template to spawn in goal objects")]
     private OnTriggerColliderFilter _goalPrefab;
-    [SerializeField]
+
+    [SerializeField, Tooltip("The prefab used as a template to spawn in obstacle objects")]
     private OnTriggerColliderFilter _obstaclePrefab;
 
+    /// <summary>
+    /// The event that fires whenever the minigame gets started.
+    /// </summary>
     [field: Header("Events")]
-    [field: SerializeField]
+    [field: SerializeField, Tooltip("The event that fires whenever the minigame gets started.")]
     public UnityEvent OnMinigameStarted { get; private set; }
-    [field: SerializeField]
+
+    /// <summary>
+    /// The event that fires whenever the time limit of the minigame has been met.
+    /// </summary>
+    [field: SerializeField, Tooltip("The event that fires whenever the time limit of the minigame has been met.")]
     public UnityEvent OnMinigameFinished { get; private set; }
 
-    [field: SerializeField]
+    /// <summary>
+    /// The event that fires whenever the pointer has touched a goal.
+    /// </summary>
+    [field: SerializeField, Tooltip("The event that fires whenever the pointer has touched a goal.")]
     public UnityEvent OnGoalTouched { get; private set; }
 
-    [field: SerializeField]
+    /// <summary>
+    /// The event that fires whenever the pointer has touched an obstacle.
+    /// </summary>
+    [field: SerializeField, Tooltip("The event that fires whenever the pointer has touched an obstacle.")]
     public UnityEvent OnObstacleTouched { get; private set; }
 
     /// <summary>
@@ -93,11 +137,14 @@ public class MinigameController : MonoBehaviour
     /// </summary>
     public void GenerateRound()
     {
-        Assert.IsNotNull(_goalPrefab);
-        Assert.IsNotNull(_obstaclePrefab);
-        Assert.IsNotNull(_pointer);
-        Assert.IsTrue(_radius > 0);
-        Assert.IsTrue(_radius > _deadzone);
+#if UNITY_ASSERTIONS
+        Assert.IsNotNull(_goalPrefab, $"{this.name} does not have a goal prefab set.");
+        Assert.IsNotNull(_obstaclePrefab, $"{this.name} does not have an obstacle prefab set.");
+        Assert.IsNotNull(_pointer, $"{this.name} does not have a pointer set and can't register when a goal or obstacle has been touched.");
+        Assert.IsTrue(_radius > 0, $"{this.name} has an invalid radius. The spawn radius must be higher than 0.");
+        Assert.IsTrue(_deadzone >= 0, $"{this.name} has an invalid deadzone. The deadzone radius must be at least 0.");
+        Assert.IsTrue(_radius > _deadzone, $"{this.name} has an invalid deadzone. The deadzone must be smaller than the spawn radius.");
+#endif
 
         ClearSpawnedObjects();
 
@@ -111,35 +158,43 @@ public class MinigameController : MonoBehaviour
             int iterations = default;
             do
             {
+                isInvalid = false;
+
                 if (iterations++ > 100)
                 {
-                    Debug.LogError("Oh shit, er gaat iets goed mis of ik heb heel veel pech.");
+                    Debug.LogWarning("Exceeded max iteration count for a randomized position.");
+
+                    // if the goal position has not been created keep on forcing it
+                    if (i == 0)
+                    {
+                        continue;
+                    }
+
                     break;
                 }
-
-                isInvalid = false;
 
                 pos = GenerateRandomPoint();
                 Vector3 worldPos = pos + transform.position;
 
-               
-
-                if (Physics.CheckSphere(worldPos, radius, -1, QueryTriggerInteraction.Collide))
+                // prevent spawning inside of the flange
+                if (IsOverlappingSphere(worldPos, radius, _pointer.transform.position, _pointer.bounds.extents.x * 1.2f))
                 {
                     isInvalid = true;
                     continue;
                 }
 
-                int childrenCount = _authoredChildren.Count;
                 // check previously generated points to prevent overlap
+                int childrenCount = _authoredChildren.Count;
                 for (int j = 0; j < childrenCount; j++)
                 {
-                    if (IsInsideRange(worldPos, radius, _authoredChildren[j].transform.position, radius))
+                    if (IsOverlappingSphere(worldPos, radius, _authoredChildren[j].transform.position, radius))
                     {
                         isInvalid = true;
                         break;
                     }
                 }
+
+                // if the state is invalid, stop early and iterate again
                 if (isInvalid)
                     continue;
 
@@ -213,7 +268,15 @@ public class MinigameController : MonoBehaviour
         return randomPoint;
     }
 
-    private static bool IsInsideRange(Vector3 posA, float radiusA, Vector3 posB, float radiusB)
+    /// <summary>
+    /// Determines if two spheres are overlapping.
+    /// </summary>
+    /// <param name="posA"> The position of the first sphere.</param>
+    /// <param name="radiusA"> The radius of the first sphere.</param>
+    /// <param name="posB"> The position of the first sphere.</param>
+    /// <param name="radiusB"> The radius of the first sphere.</param>
+    /// <returns> True if two spheres overlap.</returns>
+    private static bool IsOverlappingSphere(Vector3 posA, float radiusA, Vector3 posB, float radiusB)
     {
         Vector3 difference = posA - posB;
         return difference.sqrMagnitude < (radiusA * radiusA) + (radiusB * radiusB);
--- Assets/Scripts/Robotics/Minigame/MinigameScoreboard.cs
+++ Assets/Scripts/Robotics/Minigame/MinigameScoreboard.cs
@@ -6,34 +6,43 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+/// <summary>
+/// Displays the statistics of the minigame session on a canvas and keeps it up to date.
+/// </summary>
 [DisallowMultipleComponent]
 public class MinigameScoreboard : MonoBehaviour
 {
-    [SerializeField]
+    [SerializeField, Tooltip("The minigame that will be observed by this scoreboard")]
     MinigameController _minigame;
-    [SerializeField]
+
+    [SerializeField, Tooltip("The text mesh that will be used to display the current remaining time.")]
     TextMeshProUGUI _timeRemainingMesh;
-    [SerializeField]
+
+    [SerializeField, Tooltip("The text mesh that will be used to display the current score.")]
     TextMeshProUGUI _scoreMesh;
 
-    [SerializeField]
+    [SerializeField, Tooltip("The image that acts as a visual count down by redusing the circular fill.")]
     Image _radialIndicator;
 
+    /// <summary>
+    /// Sets up the scoreboard to listen to when the minigame gets finished.
+    /// </summary>
     private void Start()
     {
 #if UNITY_ASSERTIONS
-        Assert.IsNotNull(_minigame);
-        Assert.IsNotNull(_radialIndicator);
-        Assert.IsNotNull(_timeRemainingMesh);
-        Assert.IsNotNull(_scoreMesh);
+        Assert.IsNotNull(_minigame, $"{this.name} does not have a minigame set to observe.");
+        Assert.IsNotNull(_radialIndicator, $"{this.name} does not have an image set to manipulate the fill for.");
+        Assert.IsNotNull(_timeRemainingMesh, $"{this.name} does not have a text mesh pro instance set to write the remaining time to.");
+        Assert.IsNotNull(_scoreMesh, $"{this.name} does not have a text mesh pro instance set to write the score to.");
 #endif
 
         // make sure the GUI is updated one last time when the minigame is finished
         _minigame.OnMinigameFinished.AddListener(UpdateGUI);
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Refreshes the GUI only when the minigame is being played.
+    /// </summary>
     void Update()
     {
         if (_minigame.IsBeingPlayed)
@@ -42,15 +51,20 @@ public class MinigameScoreboard : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Updates the GUI with the current score and remaining time.
+    /// </summary>
     public void UpdateGUI()
     {
         // set the radial fill using the remaining time
         _radialIndicator.fillAmount = _minigame.TimeRemaining / _minigame.TimeLimit;
 
-        _timeRemainingMesh.text = _minigame.TimeRemaining == 0 ? 
-            _minigame.TimeRemaining.ToString() : 
+        // display the current remaining time with 1 decimal unless it's over
+        _timeRemainingMesh.text = _minigame.TimeRemaining == 0 ?
+            _minigame.TimeRemaining.ToString() :
             _minigame.TimeRemaining.ToString("0.0");
 
+        // display the current score
         _scoreMesh.text = _minigame.Score.ToString();
     }
 }
