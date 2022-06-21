384e7501282cdc175c951d0e34956fed35b9c6ff
VRStudent <luca.guagliardo@live.nl>
2022-06-21 12:08:01 +0200

    documented looking scripts

--- Assets/Scripts/Onboarding/AssistantLookTowardsPlayer.cs
+++ Assets/Scripts/Onboarding/AssistantLookTowardsPlayer.cs
@@ -2,27 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Makes sure the assistant will look towards the player.
+/// </summary>
 public class AssistantLookTowardsPlayer : MonoBehaviour
 {
-    private int assistantOffset = 90;
-    private bool active = true;
+    /// <summary>
+    /// The angle offset to make the robot actually face towards the player.
+    /// </summary>
+    private const int _assistantOffset = 90;
 
-    [SerializeField]
+    /// <summary>
+    /// Determines if the assistant should be looking at the player at this moment.
+    /// </summary>
+    private bool _active = true;
+
+    /// <summary>
+    /// The target the robot will look at.
+    /// </summary>
+    /// <remarks> Will be the main camera by default.</remarks>
+    [SerializeField, Tooltip("The target the robot will look at. Will be the main camera by default.")]
     Transform _target;
 
-    [SerializeField]
+    /// <summary>
+    /// The range of vertical rotation the assistant's head will make.
+    /// </summary>
+    [SerializeField, Tooltip("The range of vertical rotation the assistant's head will make.")]
     MinMaxFloat _headVerticalRotationBounds;
 
+    /// <summary>
+    /// Registers the main camera as target if no override is given in <see cref="_target"/>.
+    /// </summary>
     private void Awake()
     {
         if (_target == null)
             _target = Camera.main.transform;
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Sets the rotation of the assistant to look at the target when this behavior is active.
+    /// </summary>
     void LateUpdate()
     {
-        if (active)
+        if (_active)
         {
             // make the head look at the target
             transform.LookAt(_target);
@@ -32,13 +54,16 @@ public class AssistantLookTowardsPlayer : MonoBehaviour
             oldRotation.x = Mathf.Clamp(oldRotation.x > 180 ? (oldRotation.x - 360) : oldRotation.x, _headVerticalRotationBounds.Min, _headVerticalRotationBounds.Max);
 
             // swap x & z due to weird model rotations
-            transform.localRotation = Quaternion.Euler(oldRotation.z, oldRotation.y + assistantOffset, oldRotation.x);
+            transform.localRotation = Quaternion.Euler(oldRotation.z, oldRotation.y + _assistantOffset, oldRotation.x);
         }
     }
 
+    /// <summary>
+    /// Sets the state for looking at the player.
+    /// </summary>
+    /// <param name="activate"> True if the assistant should be looking at the player. False to stop looking at the player.</param>
     public void Active(bool activate)
     {
-        active = activate;
+        _active = activate;
     }
-
 }
